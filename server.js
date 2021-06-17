ee055addf0c22dd27ffc12f5c5a6489c822584ee
Erich Sauer <erich.sauer@gmail.com>
2021-06-17 13:36:18 -0700

    merge

--- server.js
+++ server.js
@@ -22,21 +22,13 @@ const namespace = io.of('/');
 app.use(express.json());
 
 // const rooms = io.sockets.adapter.rooms
-<<<<<<< HEAD
 let roomNumber = 10001;
-=======
-let num = 10001;
->>>>>>> 3c866e75a601b28c7356b58415460201e4b97f8f
 let rooms = {};
 let room = '';
 
 io.on('connection', (socket) => {
   console.log(`new connection id ${socket.id}`);
-<<<<<<< HEAD
   io.to(socket.id).emit('set userId', socket.id);
-=======
-  io.to(socket.id).emit('user id', socket.id);
->>>>>>> 3c866e75a601b28c7356b58415460201e4b97f8f
 
   // triggered by 'enter room' event on front end
   socket.on('set roomId & join', ({ userId, customRoomId }) => {
@@ -46,20 +38,12 @@ io.on('connection', (socket) => {
     roomId = customRoomId ? customRoomId : 'room' + roomNumber;
     console.log('custom room id', roomId);
 
-<<<<<<< HEAD
     socket.join(roomId);
-=======
-  socket.on('collab', (customRoom) => {
-    room = customRoom ? customRoom : 'room' + num;
-
-    if (!rooms[room]) rooms[room] = {};
->>>>>>> 3c866e75a601b28c7356b58415460201e4b97f8f
 
     const numOfParticipants = Array.from(
       namespace.adapter.rooms.get(roomId)
     ).length;
 
-<<<<<<< HEAD
     if (!customRoomId && numOfParticipants >= 3) {
       roomNumber++;
     }
@@ -71,15 +55,6 @@ io.on('connection', (socket) => {
         ...rooms[roomId],
         [userId]: '',
       };
-=======
-    io.in(room).emit('set room', { room, users: rooms[room] });
-
-    const numOfParticipants = Array.from(
-      namespace.adapter.rooms.get(room)
-    ).length;
-
-    if (numOfParticipants >= 3) num++;
->>>>>>> 3c866e75a601b28c7356b58415460201e4b97f8f
 
     console.log('from set roomId', rooms);
 
@@ -93,28 +68,17 @@ io.on('connection', (socket) => {
     io.in(roomId).emit('state from server', users);
   });
 
-<<<<<<< HEAD
   socket.on('set color', ({ roomId, user }) => {
     console.log('set color', roomId, user);
     rooms[roomId] = {
       ...rooms[roomId],
       ...user,
     };
-=======
-  socket.on('set color', ({ room, user }) => {
-    rooms[room] = {
-      ...rooms[room],
-      ...user,
-    };
-    io.in(room).emit('state from server', rooms[room]);
-  });
->>>>>>> 3c866e75a601b28c7356b58415460201e4b97f8f
 
     const users = rooms[roomId];
     io.in(roomId).emit('state from server', users);
   });
 
-<<<<<<< HEAD
   socket.on('begin', (roomId) => {
     io.in(roomId).emit('close modal');
     roomNumber++;
@@ -129,8 +93,6 @@ io.on('connection', (socket) => {
   //   io.in(room).emit('num participants', numOfParticipants);
   // });
 
-=======
->>>>>>> 3c866e75a601b28c7356b58415460201e4b97f8f
   socket.on('client chat', ({ input, color }, socketRoom) => {
     io.in(socketRoom).emit('server chat', { input, color });
   });
